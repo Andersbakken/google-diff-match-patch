a92e7ad86ca6779e3d08600f76923fabccc7c493
Anders Bakken <agbakken@gmail.com>
2013-07-25 08:51:13 -0400

    Tada

--- cpp/QtCore.h
+++ cpp/QtCore.h
@@ -371,48 +371,48 @@ public:
         : pattern(p)
     {}
 
-    int indexIn(const QString &string) const;
-    // {
-    //     regex_t regex;
-    //     if (regcomp(&regex, pattern.constData(), 0))
-    //         return -1;
+    int indexIn(const QString &string) const
+    {
+        regex_t regex;
+        if (regcomp(&regex, pattern.constData(), 0))
+            return -1;
 
-    //     int ret = -1;
-    //     regmatch_t captures[10];
-    //     if (!regexec(&regex, string.constData(), sizeof(captures) / sizeof(regmatch_t), captures, 0)) {
-    //         for (int i=0; i<10 && captures[i].rm_so != -1; ++i) {
-    //             printf("%d: %lld %lld\n", i, captures[i].rm_so, captures[i].rm_eo);
-    //             caps.append(string.mid(captures[i].rm_so, captures[i].rm_eo - captures[i].rm_so));
-    //         }
+        int ret = -1;
+        regmatch_t captures[10];
+        if (!regexec(&regex, string.constData(), sizeof(captures) / sizeof(regmatch_t), captures, 0)) {
+            for (int i=0; i<10 && captures[i].rm_so != -1; ++i) {
+                printf("%d: %lld %lld\n", i, captures[i].rm_so, captures[i].rm_eo);
+                caps.append(string.mid(captures[i].rm_so, captures[i].rm_eo - captures[i].rm_so));
+            }
 
-    //         ret = captures[0].rm_so;
-    //     }
+            ret = captures[0].rm_so;
+        }
 
-    //     regfree(&regex);
+        regfree(&regex);
 
-    //     return ret;
-    // }
+        return ret;
+    }
 
-    bool exactMatch(const QString &string) const;
-    // {
-    //     regex_t regex;
-    //     if (regcomp(&regex, pattern.constData(), 0))
-    //         return -1;
+    bool exactMatch(const QString &string) const
+    {
+        regex_t regex;
+        if (regcomp(&regex, pattern.constData(), 0))
+            return -1;
 
-    //     bool ret = false;
-    //     regmatch_t captures[1];
-    //     if (!regexec(&regex, string.constData(), sizeof(captures) / sizeof(regmatch_t), captures, 0)) {
-    //         ret = captures[0].rm_so == 0 && captures[0].rm_eo == string.size();
-    //         for (int i=0; i<10 && captures[i].rm_so != -1; ++i) {
-    //             printf("%d: %lld %lld\n", i, captures[i].rm_so, captures[i].rm_eo);
-    //             caps.append(string.mid(captures[i].rm_so, captures[i].rm_eo - captures[i].rm_so));
-    //         }
-    //     }
+        bool ret = false;
+        regmatch_t captures[1];
+        if (!regexec(&regex, string.constData(), sizeof(captures) / sizeof(regmatch_t), captures, 0)) {
+            ret = captures[0].rm_so == 0 && captures[0].rm_eo == string.size();
+            for (int i=0; i<10 && captures[i].rm_so != -1; ++i) {
+                printf("%d: %lld %lld\n", i, captures[i].rm_so, captures[i].rm_eo);
+                caps.append(string.mid(captures[i].rm_so, captures[i].rm_eo - captures[i].rm_so));
+            }
+        }
 
-    //     regfree(&regex);
+        regfree(&regex);
 
-    //     return ret;
-    // }
+        return ret;
+    }
 
     QString cap(int idx) const
     {
